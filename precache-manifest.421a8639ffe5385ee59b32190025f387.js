e5f78a5a69a98d9123d84e0e187d439a349db800
Masao Kitamura <masaok@gmail.com>
2019-07-29 23:24:45 -0700

    Updates

---
+++ precache-manifest.421a8639ffe5385ee59b32190025f387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0c71d006ac3c74ad8fd12231638d49",
+    "url": "/masao-react/index.html"
+  },
+  {
+    "revision": "a260fa17edf8170af6a7",
+    "url": "/masao-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/masao-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a260fa17edf8170af6a7",
+    "url": "/masao-react/static/js/main.3813fb83.chunk.js"
+  },
+  {
+    "revision": "57a1f6c36683a4b2bdf3",
+    "url": "/masao-react/static/js/runtime~main.a4b06090.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/masao-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
